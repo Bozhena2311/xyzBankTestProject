3121134f08a5a975b66e8b810f711ee5e9e1ee46
Bozhena Balzhanova <bozhena.balzhanova@gmail.com>
2022-06-26 19:07:54 +0300

    add dependency for allure, add annotation steps for allure

--- src/main/java/Flow/LoginFlow.java
+++ src/main/java/Flow/LoginFlow.java
@@ -1,36 +1,43 @@
 package Flow;
 
 import Pages.LoginPage;
+import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Selenide.page;
 
 public class LoginFlow {
 
+    @Step("Click button - Customer Login")
     public LoginFlow clickLoginLikeCustomerButton() {
         page(LoginPage.class).getCustomerLoginButton().click();
         return this;
     }
 
+    @Step("Click dropDown customer list")
     public LoginFlow clickDropDownList() {
         page(LoginPage.class).getChangeNameCustomerDropDownField().click();
         return this;
     }
 
+    @Step("Select customer - Germoine Granger")
     public LoginFlow selectHermoineGranger() {
         page(LoginPage.class).getHermoineGrangerSelectionField().click();
         return this;
     }
 
+    @Step("Select customer - Harry Potter")
     public LoginFlow selectHarryPotter() {
         page(LoginPage.class).getHarryPotterSelectionField().click();
         return this;
     }
 
+    @Step("Click button - Login")
     public LoginFlow clickLoginButton() {
         page(LoginPage.class).getLoginButton().click();
         return this;
     }
 
+    @Step("Go to Main Flow")
     public MainFlow goToMainFlow() {
         return new MainFlow();
     }
--- src/test/java/LoginAsCustomerHermoineTest.java
+++ src/test/java/LoginAsCustomerHermoineTest.java
@@ -1,10 +1,12 @@
 import Base.BaseTest;
 import Flow.LoginFlow;
+import io.qameta.allure.Owner;
 import org.testng.annotations.Test;
 
 public class LoginAsCustomerHermoineTest extends BaseTest {
 
-    @Test
+    @Test(description = "TC1. Auth like customer - Hermoine Granger")
+    @Owner("BalzhanovaBM")
     void loginAsHermoineGranger() {
         new LoginFlow()
                 .clickLoginLikeCustomerButton()
