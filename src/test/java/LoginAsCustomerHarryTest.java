f4022b1f99f4bf5f90916fc2199e747b1ac37a85
Bozhena Balzhanova <bozhena.balzhanova@gmail.com>
2022-06-26 19:16:40 +0300

    add annotation for allure in test

--- src/test/java/LoginAsCustomerHarryTest.java
+++ src/test/java/LoginAsCustomerHarryTest.java
@@ -1,10 +1,12 @@
 import Base.BaseTest;
 import Flow.LoginFlow;
+import io.qameta.allure.Owner;
 import org.testng.annotations.Test;
 
 public class LoginAsCustomerHarryTest extends BaseTest {
 
-    @Test()
+    @Test(description = "TC2. Auth like customer - Harry Potter")
+    @Owner("BalzhanovaBM")
     void loginAsHarryPotter() {
         new LoginFlow()
                 .clickLoginLikeCustomerButton()
